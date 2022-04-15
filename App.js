c0932ae2a5db00f5a864bd98d7f3f0231f7dc9e0
M. Ammar Fadhila Ramadhan <ammarramadhan01@gmail.com>
2022-04-15 13:30:30 +0700

    First Commit Expo

--- App.js
+++ App.js
@@ -1,20 +1,144 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import { Grid, Col, Row } from 'react-native-easy-grid';
+import { Magnetometer } from 'expo-sensors';
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
+const { height, width } = Dimensions.get('window');
+
+export default App = () => {
+
+  const [subscription, setSubscription] = useState(null);
+  const [magnetometer, setMagnetometer] = useState(0);
+
+  useEffect(() => {
+    _toggle();
+    return () => {
+      _unsubscribe();
+    };
+  }, []);
+
+  const _toggle = () => {
+    if (subscription) {
+      _unsubscribe();
+    } else {
+      _subscribe();
+    }
+  };
+
+  const _subscribe = () => {
+    setSubscription(
+      Magnetometer.addListener((data) => {
+        setMagnetometer(_angle(data));
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  const _angle = (magnetometer) => {
+    let angle = 0;
+    if (magnetometer) {
+      let { x, y, z } = magnetometer;
+      if (Math.atan2(y, x) >= 0) {
+        angle = Math.atan2(y, x) * (180 / Math.PI);
+      } else {
+        angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
+      }
+    }
+    return Math.round(angle);
+  };
+
+  const _direction = (degree) => {
+    if (degree >= 22.5 && degree < 67.5) {
+      return 'NE';
+    }
+    else if (degree >= 67.5 && degree < 112.5) {
+      return 'E';
+    }
+    else if (degree >= 112.5 && degree < 157.5) {
+      return 'SE';
+    }
+    else if (degree >= 157.5 && degree < 202.5) {
+      return 'S';
+    }
+    else if (degree >= 202.5 && degree < 247.5) {
+      return 'SW';
+    }
+    else if (degree >= 247.5 && degree < 292.5) {
+      return 'W';
+    }
+    else if (degree >= 292.5 && degree < 337.5) {
+      return 'NW';
+    }
+    else {
+      return 'N';
+    }
+  };
+
+  const _degree = (magnetometer) => {
+    return magnetometer - 90 >= 0 ? magnetometer - 90 : magnetometer + 271;
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Grid style={{ backgroundColor: 'black' }}>
+      <Row style={{ alignItems: 'center' }} size={.9}>
+        <Col style={{ alignItems: 'center' }}>
+          <Text
+            style={{
+              color: '#32CD32',
+              fontSize: height / 26,
+              fontWeight: 'bold'
+            }}>
+            {_direction(_degree(magnetometer))}
+          </Text>
+        </Col>
+      </Row>
+
+      <Row style={{ alignItems: 'center' }} size={.1}>
+        <Col style={{ alignItems: 'center' }}>
+          <View style={{ position: 'absolute', width: width, alignItems: 'center', top: 0 }}>
+            <Image source={require('./assets/compass_pointer.png')} style={{
+              height: height / 26,
+              resizeMode: 'contain'
+            }} />
+          </View>
+        </Col>
+      </Row>
+
+      <Row style={{ alignItems: 'center' }} size={2}>
+        <Text style={{
+          color: '#32CD32',
+          fontSize: height / 27,
+          width: width,
+          position: 'absolute',
+          textAlign: 'center'
+        }}>
+          {_degree(magnetometer)}°
+          </Text>
+      </Row>
+
+      <Row style={{ alignItems: 'center' }} size={1}>
+        <Col style={{ alignItems: 'center' }}>
+          <Text style={{ 
+            color: '#32CD32' 
+            }}>
+              M. Ammar Fadhila Ramadhan</Text>
+          <Text style={{ 
+            color: '#32CD32' 
+            }}>
+              119140029</Text>
+          <Text style={{ 
+            color: '#32CD32'
+             }}>
+               Put the device on flat horizontal platform</Text>
+        </Col>
+      </Row>
+
+    </Grid>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
